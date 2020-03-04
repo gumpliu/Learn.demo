ba47ae8f20db19fe1da5dec03a3a4b5b4978abe7
liushengpeng <liushengpeng@ysstech.com>
2020-03-04 09:54:48 +0800

    contextFilter 维护context并委托给FSIPContextFactory

--- fsip/com.yss.fsip.parent/com.yss.fsip.web/src/main/java/com/yss/fsip/web/filter/ContextFilter.java
+++ fsip/com.yss.fsip.parent/com.yss.fsip.web/src/main/java/com/yss/fsip/web/filter/ContextFilter.java
@@ -1,42 +1,38 @@
 package com.yss.fsip.web.filter;
 
-import com.yss.fsip.context.FSIPContext;
 import com.yss.fsip.context.FSIPContextFactory;
 import com.yss.fsip.util.BeanUtil;
 import com.yss.fsip.web.context.FSIPWebContextFactory;
 import com.yss.fsip.web.context.FSIPWebContextFactoryImpi;
-import com.yss.fsip.web.utils.CookieUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.annotation.Order;
-import org.springframework.util.StringUtils;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
  * 维护FSIPContext上下文
- * 
+ *
  * @author LSP
  *
  */
 @Order
 @WebFilter(filterName = "contextFilter", urlPatterns = "/*")
 public class ContextFilter implements Filter {
-	
+
 	private Logger logger = LoggerFactory.getLogger(ContextFilter.class);
+
+	private volatile FSIPWebContextFactory fsipWebContextFactory = null;
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 
-		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-		if(StringUtils.isEmpty(CookieUtils.getSessionIdCookie(httpServletRequest))) {
-			FSIPContext fsipContext = getContextFactory(request).createContext();
-			fsipContext.getUserId();
-		}
+		//创建上下文信息
+		getContextFactory(request).createContext();
+
 		chain.doFilter(request, response);
 	}
 	//兼容filter 低版本，低版本没有默认实现
@@ -54,14 +50,19 @@ public class ContextFilter implements Filter {
 
 	private FSIPContextFactory getContextFactory(ServletRequest request){
 
-		FSIPWebContextFactory factory = BeanUtil.getBean(FSIPWebContextFactory.class);
+		if(fsipWebContextFactory == null){
+			synchronized (this){
+				if(fsipWebContextFactory == null) {
+					fsipWebContextFactory = BeanUtil.getBean(FSIPWebContextFactory.class);
 
-		if(factory == null){
-			factory = new FSIPWebContextFactoryImpi();
+					if (fsipWebContextFactory == null) {
+						fsipWebContextFactory = new FSIPWebContextFactoryImpi();
+					}
+					fsipWebContextFactory.setRequest(request);
+				}
+			}
 		}
 
-		factory.setRequest(request);
-
-		return factory;
+		return fsipWebContextFactory;
 	}
 }
