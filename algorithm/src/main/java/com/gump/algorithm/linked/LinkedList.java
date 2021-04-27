be133f90c1ffe2780d50ba22fc2252bc74d83164
liushengpeng <liushengpeng@ysstech.com>
2021-04-27 17:59:53 +0800

    合并链表

--- algorithm/src/main/java/com/gump/algorithm/linked/LinkedList.java
+++ algorithm/src/main/java/com/gump/algorithm/linked/LinkedList.java
@@ -12,6 +12,10 @@ import java.util.List;
  */
 public class LinkedList {
 
+  public static void main(String[] args) {
+    List<ListNode> listNodes = new ArrayList<>();
+  }
+
   public List<Integer> printListFromTailToHead(ListNode listNode) {
 
     if(listNode == null) return Collections.emptyList();
@@ -46,12 +50,68 @@ public class LinkedList {
     }
     return pre;
   }
+
+  public ListNode mergeKLists(ArrayList<ListNode> lists) {
+    if(lists == null || lists.size() == 0) return null;
+    //
+    while (lists.size() > 1){
+      ListNode node1 = lists.get(lists.size() - 1);
+      ListNode node2 = lists.get(lists.size() - 2);
+      lists.remove(lists.size() - 1);
+      lists.remove(lists.size() - 2);
+
+      ListNode meger = mergeKLists(node1, node2);
+      lists.add(lists.size() - 1, meger);
+    }
+
+
+    return lists.get(0);
+  }
+
+  public ListNode mergeKLists(ListNode node1, ListNode node2) {
+
+    if(node1 == null) return node2;
+    if (node2 == null) return node1;
+
+     ListNode heard = new ListNode();
+
+    while (node1 != null && node2 != null){
+      if(node1.num >= node2.num){
+        ListNode next = node2.next;
+        node2.next = null;
+        heard.next = node2;
+        heard = node2;
+        node2 = next;
+      }else{
+        ListNode next = node1.next;
+        node1.next = null;
+        heard.next = node1;
+        heard = node1;
+        node1 = next;
+      }
+    }
+
+    if(node1 != null){
+      heard.next = node1;
+    }
+    if (node2 != null){
+      heard.next = node2;
+    }
+
+    return heard.next;
+  }
+
+
 }
 
 
 class ListNode{
   public   int num;
   public ListNode next;
+
+  public ListNode(){
+  }
+
 
   public ListNode(int num){
     this.num = num;
--- algorithm/src/main/java/com/gump/algorithm/strring/StringOperation.java
+++ algorithm/src/main/java/com/gump/algorithm/strring/StringOperation.java
@@ -1,5 +1,9 @@
 package com.gump.algorithm.strring;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 /**
  * @Description: 字符串操作
  * @Author gumpLiu
@@ -9,7 +13,9 @@ package com.gump.algorithm.strring;
 public class StringOperation {
 
     public static void main(String[] args) {
-        solve("abcd");
+//        [1,2,4,4,5],4
+        int[] num = {-2,-2,-2};
+        search(num, -2);
     }
 
     /**
@@ -28,6 +34,97 @@ public class StringOperation {
         }
         return new String(chars);
     }
+
+
+    /**
+     * 给出一个仅包含字符'(',')','{','}','['和']',的字符串，判断给出的字符串是否是合法的括号序列
+     * 括号必须以正确的顺序关闭，"()"和"()[]{}"都是合法的括号序列，但"(]"和"([)]"不合法。
+     * @param s string字符串
+     * @return bool布尔型
+     */
+    public static boolean isValid (String s) {
+        char[] chars = s.toCharArray();
+        Stack stack = new Stack();
+        for(char c : chars){
+            switch (c){
+                case '(' :
+                case '{' :
+                case '[' :
+                    stack.push(c);
+                    break;
+                case ')' :
+                     if(stack.empty()) return false;
+                     if(!stack.pop().equals('(')){
+                         return false;
+                     }
+                    break;
+                case '}' :
+                    if(stack.empty()) return false;
+                    if(!stack.pop().equals('{')){
+                        return false;
+                    }
+                    break;
+                case ']' :
+                    if(stack.empty()) return false;
+                    if(!stack.pop().equals('[')){
+                        return false;
+                    }
+                    break;
+                default:
+                    return false;
+            }
+
+        }
+        return stack.empty();
+    }
+
+
+    /**
+     * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+     *
+     * 请实现有重复数字的升序数组的二分查找
+     * 给定一个 元素有序的（升序）整型数组 nums 和一个目标值 target  ，
+     * 写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1
+     *
+     * 如果目标值存在返回下标，否则返回 -1
+     * @param nums int整型一维数组
+     * @param target int整型
+     * @return int整型
+     */
+    public static int search (int[] nums, int target) {
+        // write code here
+        if(nums == null || nums.length == 0) return -1;
+
+
+        int start = 0;
+        int end = nums.length;
+        int index = -1;
+        while (start <= end){
+            int mod = (end + start)/2;
+            if(nums[mod] == target){
+                index = mod;
+                break;
+            }else if(nums[mod] > target){
+                end = mod - 1;
+            }else if(nums[mod] < target){
+                start = mod + 1;
+            }
+        }
+
+        if(index == -1) return -1;
+
+        for(int i = index - 1; i>=0; i--){
+            if(nums[i] == target){
+                --index;
+            }else {
+                return i + 1;
+            }
+
+        }
+
+        return index;
+    }
+
 
     public static void swap(int i, int j, char[] num){
         char c = num[i];
